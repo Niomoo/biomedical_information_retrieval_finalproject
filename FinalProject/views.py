ec1cf7230ec98135ca4613ef035cadcf01177e82
niomoo <jennyliu.lyh@gmail.com>
2023-01-03 00:00:06 +0800

    [Feature]Add search MeSHterm feature

--- FinalProject/views.py
+++ FinalProject/views.py
@@ -19,8 +19,9 @@ from collections import Counter
 from sklearn.metrics.pairwise import linear_kernel
 from BIR.settings import PROJECT_ROOT
 import networkx as nx
-import matplotlib.pyplot as plt
 import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import io
 
 search_info = []
@@ -33,7 +34,8 @@ class searchInfo:
         self.doc_content = doc_content
 
 def index(request):
-    image_bytes = get_covid_img()
+    sorted_list = get_sorted_MeSHTerm()
+    image_bytes = get_mesh_img('covid', sorted_list[:10])
     return render(request, 'index.html', {
         'covid_img': image_bytes,
     })
@@ -46,7 +48,7 @@ def get_index(sort_list, key):
         if item[0] == key:
             return i
 
-def get_covid_img():
+def get_sorted_MeSHTerm():
     filepath = os.path.join(PROJECT_ROOT, 'covid_data.json')
     MeSHterm = {}
     with open(filepath) as file:
@@ -59,16 +61,17 @@ def get_covid_img():
                     else:
                         MeSHterm[terms] += article['MeSHterm'][terms]         
     sorted_MeSHterm = sorted(MeSHterm.items(), key=lambda x: x[1], reverse=True)
-    covid_list = sorted_MeSHterm[:10]
+    return sorted_MeSHterm
+
+def get_mesh_img(key_word, draw_list):
 
     fig, ax = plt.subplots(figsize=(15, 8))
 
-    key_word = 'covid'
     relationships = pd.DataFrame({'from': [key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word], 
-                'to':   covid_list})
+                'to':   draw_list})
 
     # Create DF for node characteristics
-    carac = pd.DataFrame({'ID':[key_word, covid_list[0], covid_list[1], covid_list[2], covid_list[3], covid_list[4], covid_list[5], covid_list[6], covid_list[7], covid_list[8], covid_list[9]], 
+    carac = pd.DataFrame({'ID':[key_word, draw_list[0], draw_list[1], draw_list[2], draw_list[3], draw_list[4], draw_list[5], draw_list[6], draw_list[7], draw_list[8], draw_list[9]], 
                 'type':['point', 'big', 'big', 'big', 'big', 'mid', 'mid', 'mid', 'sml', 'sml', 'sml']})
 
     # Create graph object
@@ -102,7 +105,22 @@ def search(request):
         pos_5 = []
         neg_5 = []
         if way == "method_mesh":
-            pass
+            filepath = os.path.join(PROJECT_ROOT, 'covid_data.json')
+            MeSHterm = {}
+            with open(filepath) as file:
+                jsondata = json.loads(file.read())
+                for article in jsondata:
+                    if len(article['MeSHterm']) != 0:
+                        if key_word in article['MeSHterm']:
+                            for terms in article['MeSHterm']:                          
+                                if terms not in MeSHterm:
+                                    MeSHterm[terms] = 1
+                                else:
+                                    MeSHterm[terms] += article['MeSHterm'][terms]            
+            sorted_mesh = sorted(MeSHterm.items(), key=lambda x: x[1], reverse=True)
+            index = get_index(sorted_mesh, key_word)
+            image_bytes = get_mesh_img(key_word, sorted_mesh[index:index+10])
+            pos_5 = sorted_mesh[index:index+5]
         elif way == "method_sg":
             model = word2vec.Word2Vec.load(os.path.join(PROJECT_ROOT, 'sg_depression_data.model'))
 
@@ -128,41 +146,42 @@ def search(request):
         # Draw graph
         fig, ax = plt.subplots(figsize=(15, 8))
 
-        relationships = pd.DataFrame({'from': [key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word], 
-                 'to':   [pos_5[0][0], pos_5[1][0], pos_5[2][0], pos_5[3][0], pos_5[4][0], neg_5[0][0], neg_5[1][0], neg_5[2][0], neg_5[3][0], neg_5[4][0]]})
+        if way == 'method_mesh':
+            pass
+        else:
+            relationships = pd.DataFrame({'from': [key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word, key_word], 
+                    'to':   [pos_5[0][0], pos_5[1][0], pos_5[2][0], pos_5[3][0], pos_5[4][0], neg_5[0][0], neg_5[1][0], neg_5[2][0], neg_5[3][0], neg_5[4][0]]})
 
-        # Create DF for node characteristics
-        carac = pd.DataFrame({'ID':[key_word, pos_5[0][0], pos_5[1][0], pos_5[2][0], pos_5[3][0], pos_5[4][0], neg_5[0][0], neg_5[1][0], neg_5[2][0], neg_5[3][0], neg_5[4][0]], 
-                    'type':['points', 'number', 'number', 'number', 'number', 'number', 'Letter', 'Letter', 'Letter', 'Letter', 'Letter']})
+            # Create DF for node characteristics
+            carac = pd.DataFrame({'ID':[key_word, pos_5[0][0], pos_5[1][0], pos_5[2][0], pos_5[3][0], pos_5[4][0], neg_5[0][0], neg_5[1][0], neg_5[2][0], neg_5[3][0], neg_5[4][0]], 
+                        'type':['points', 'number', 'number', 'number', 'number', 'number', 'Letter', 'Letter', 'Letter', 'Letter', 'Letter']})
 
-        # Create graph object
-        G = nx.from_pandas_edgelist(relationships, 'from', 'to', create_using=nx.Graph())
+            # Create graph object
+            G = nx.from_pandas_edgelist(relationships, 'from', 'to', create_using=nx.Graph())
 
-        # Make types into categories
-        carac = carac.set_index('ID')
-        carac = carac.reindex(G.nodes())
+            # Make types into categories
+            carac = carac.set_index('ID')
+            carac = carac.reindex(G.nodes())
 
-        carac['type'] = pd.Categorical(carac['type'])
-        carac['type'].cat.codes
-
-        # Specify colors
-        cmap = matplotlib.colors.ListedColormap(['C0', 'darkorange', 'red'])
-
-        # Set node sizes
-        node_sizes = 4000
-
-        # Draw graph
-        nx.draw(G, with_labels=True, node_color=carac['type'].cat.codes, cmap=cmap, 
-                node_size=node_sizes)
-        buf = io.BytesIO()
-        plt.savefig(buf, format='svg', bbox_inches='tight')
-        image_bytes = buf.getvalue().decode('utf-8')
-        buf.close()
-        plt.close()
+            carac['type'] = pd.Categorical(carac['type'])
+            carac['type'].cat.codes
+            # Specify colors
+            cmap = matplotlib.colors.ListedColormap(['C0', 'darkorange', 'red'])
+            # Set node sizes
+            node_sizes = 4000
+            # Draw graph
+            nx.draw(G, with_labels=True, node_color=carac['type'].cat.codes, cmap=cmap, 
+                    node_size=node_sizes)
+            buf = io.BytesIO()
+            plt.savefig(buf, format='svg', bbox_inches='tight')
+            image_bytes = buf.getvalue().decode('utf-8')
+            buf.close()
+            plt.close()
 
         # my_chart = image_bytes
 
-        covid_img = get_covid_img()
+        sorted_list = get_sorted_MeSHTerm()
+        covid_img = get_mesh_img('covid', sorted_list[:10])
 
         # Show the data
         with open(os.path.join(PROJECT_ROOT, 'depression_data.json'), "r", encoding="utf-8") as f:
